354b7c5859ac61e2597bfc552baa4aa0e73f4296
Icelain <xerneas965@gmail.com>
2021-01-06 10:50:38 +0530

    Update biggerpp.py

--- biggerpp.py
+++ biggerpp.py
@@ -2,7 +2,7 @@
 import webbrowser
 webbrowser.open("https://www.youtube.com/watch?v=oHg5SJYRHA0")
 
-# Above won't interfere with the program
+# Above won't interfere with the main code. It's for the best experience.
 
 import subprocess
 process = subprocess.Popen(["sh","bigpp.sh"],stdout=subprocess.PIPE)
