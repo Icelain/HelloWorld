52a93a135bb90f3ea279f035c7cb35e235da2585
Icelain <xerneas965@gmail.com>
2021-01-06 10:51:52 +0530

    Update evenbiggerpp.js

--- evenbiggerpp.js
+++ evenbiggerpp.js
@@ -1,7 +1,7 @@
 const {spawn} = require("child_process");
 const process = spawn("python3",["biggerpp.py"]);
 
-/* Haven't handled any errors here */
+/* Haven't handled any errors here, big pp move*/
 
 process.stdout.on("data",(data)=>{
 	console.log(data.toString());
